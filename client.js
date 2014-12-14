a63f4b645598ea2077155965b61fd44c399b80b0
Innpaul Zhang <zyp108421@gmail.com>
2014-12-14 21:49:23 +0800

    Configurable log level

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
-var config = require('./config.json');
+var config = require('./lib/config');
 
 var log4js = require('log4js');
 log4js.loadAppender('file');
 log4js.addAppender(log4js.appenders.file('logs/client.log'), 'client');
 
 var logger = log4js.getLogger('client');
+logger.setLevel(config.LOG_LEVEL);
 var retry = -1;
 
 var connectClient = require('./lib/client/connectClient');
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var config = require('./config.json');
+var config = require('./lib/config');
 
 var log4js = require('log4js');
 log4js.loadAppender('file');
 log4js.addAppender(log4js.appenders.file('logs/server.log'), 'server');
 
 var logger = log4js.getLogger('server');
+logger.setLevel(config.LOG_LEVEL);
 
 var connectServer = require('./lib/server/connectServer')
         (logger, config.CONNECT_PORT, config.CONNECT_SIGN),
